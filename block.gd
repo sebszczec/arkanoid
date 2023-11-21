7c24912729b5f3d6ad1f8ee5dd0d9b03bbbf9052
Sebastian Szczecina <sebszczec@wp.pl>
2023-11-21 09:51:54 +0100

    block color changing refactored

--- block.gd
+++ block.gd
@@ -1,48 +1,24 @@
 extends CharacterBody2D
 
 @export var MAX_HITS = 4
-@export var color = "green"
+@export var COLOR = "green"
 var hits = 0
 var textures = {}
 @onready var sprite2D = $Sprite2D
 @onready var animatedSprite2D = $AnimatedSprite2D
 
+func _set_color(color):
+	var temp = []
+	temp.append(load("res://resources/blocks/" + color + "/damaged/block_d1.png"))
+	temp.append(load("res://resources/blocks/" + color + "/damaged/block_d2.png"))
+	temp.append(load("res://resources/blocks/" + color + "/damaged/block_d3.png"))
+	textures[color] = temp
+	sprite2D.texture = load("res://resources/blocks/" + color + "/block_full.png")
+	
+	# more colors can be loaded if dynamic change of color will be required
+
 func _ready():
-	var blue = []
-	blue.append(load("res://resources/blocks/blue/damaged/block_d1.png"))
-	blue.append(load("res://resources/blocks/blue/damaged/block_d2.png"))
-	blue.append(load("res://resources/blocks/blue/damaged/block_d3.png"))
-	textures["blue"] = blue
-	sprite2D.texture = load("res://resources/blocks/blue/block_full.png")
-	
-	var red = []
-	red.append(load("res://resources/blocks/red/damaged/block_d1.png"))
-	red.append(load("res://resources/blocks/red/damaged/block_d2.png"))
-	red.append(load("res://resources/blocks/red/damaged/block_d3.png"))
-	textures["red"] = red
-	sprite2D.texture = load("res://resources/blocks/red/block_full.png")
-	
-	var yellow = []
-	yellow.append(load("res://resources/blocks/yellow/damaged/block_d1.png"))
-	yellow.append(load("res://resources/blocks/yellow/damaged/block_d2.png"))
-	yellow.append(load("res://resources/blocks/yellow/damaged/block_d3.png"))
-	textures["yellow"] = yellow
-	sprite2D.texture = load("res://resources/blocks/yellow/block_full.png")
-	
-	var orange = []
-	orange.append(load("res://resources/blocks/orange/damaged/block_d1.png"))
-	orange.append(load("res://resources/blocks/orange/damaged/block_d2.png"))
-	orange.append(load("res://resources/blocks/orange/damaged/block_d3.png"))
-	textures["orange"] = orange
-	sprite2D.texture = load("res://resources/blocks/orange/block_full.png")
-	
-	var green = []
-	green.append(load("res://resources/blocks/green/damaged/block_d1.png"))
-	green.append(load("res://resources/blocks/green/damaged/block_d2.png"))
-	green.append(load("res://resources/blocks/green/damaged/block_d3.png"))
-	textures["green"] = green
-	sprite2D.texture = load("res://resources/blocks/green/block_full.png")
-	
+	_set_color(COLOR)
 	animatedSprite2D.visible = false
 
 func _physics_process(delta):
@@ -57,7 +33,7 @@ func _on_hurt_box_area_entered(area):
 		return
 	
 	assert(hits < 4)
-	sprite2D.texture = textures[color][hits - 1]
+	sprite2D.texture = textures[COLOR][hits - 1]
 
 func _on_animated_sprite_2d_animation_looped():
 	animatedSprite2D.stop()
