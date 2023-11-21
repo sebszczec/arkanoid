02ffd1fc83a2d644b27edadcd0a1d6280dc2ef4d
Sebastian Szczecina <sebszczec@wp.pl>
2023-11-21 09:07:27 +0100

    more colors of block sprites added

--- ball.gd
+++ ball.gd
@@ -8,8 +8,7 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	
 	if collision:
-		ball_velocity = velocity.bounce(collision.get_normal()) 
-		+ (0.5 * collision.get_collider_velocity())
+		ball_velocity = velocity.bounce(collision.get_normal()) + (0.25 * collision.get_collider_velocity())
 		
 
 
--- block.gd
+++ block.gd
@@ -1,22 +1,33 @@
 extends CharacterBody2D
 
-const MAX_HITS = 4
+@export var MAX_HITS = 4
 var hits = 0
-
 var textures = []
+@onready var sprite2D = $Sprite2D
+@onready var animatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	textures.append(load("res://resources/blocks/blue/damaged/block_d1.png"))
 	textures.append(load("res://resources/blocks/blue/damaged/block_d2.png"))
 	textures.append(load("res://resources/blocks/blue/damaged/block_d3.png"))
+	
+	animatedSprite2D.visible = false
 
 func _physics_process(delta):
 	pass
 
 func _on_hurt_box_area_entered(area):
 	hits = hits + 1
-	if hits == 4:
-		self.queue_free()
+	if hits == MAX_HITS:	
+		sprite2D.visible = false
+		animatedSprite2D.visible = true
+		animatedSprite2D.play("Destroy")
 		return
 	
-	$Sprite2D.texture = textures[hits - 1]
+	assert(hits < 4)
+	sprite2D.texture = textures[hits - 1]
+
+func _on_animated_sprite_2d_animation_looped():
+	animatedSprite2D.stop()
+	animatedSprite2D.visible = false
+	self.queue_free()
